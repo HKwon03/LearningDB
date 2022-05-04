d21cc2d49cc3c532e2851cfec2483f500648ceb0
HKwon03 <viplayer03@gmail.com>
2022-05-04 18:49:18 +0900

    Commit - 20220504 - kosmo - Trigger, MSSQL, MYSQL

--- Oracle-Scrips/저장 프로시져 문제.sql
+++ Oracle-Scrips/저장 프로시져 문제.sql
@@ -2,24 +2,24 @@
 
 1. 각 부서별로 최소급여, 최대급여, 평균급여를 출력하는 저장프로시져를 생성하시오. 
 	[employee, department ] 테이블 이용
-    
+  
 create or replace procedure sp_min_max_avg
-is
-    v_dno number(2);
-    v_min number(7,2);
+is -- 변수 선언부, 커서 선언
+    v_dno number(2);    -- employee.dno%type;
+    v_min number(7,2);  -- employee.salary%type;
     v_max number(7,2);
     v_avg number(7,2);
     cursor c1
     is
-    select dno, min(salary), max(salary), round(avg(salary), 2)
+    select dno, min(salary), max(salary), avg(salary)
     from employee
     group by dno;
    
 begin
-    open c1;
+    open c1;    -- 커서 시작
     loop
         fetch c1 into v_dno, v_min, v_max, v_avg;
-        exit when c1%notfound;  
+        exit when c1%notfound;   -- 레코드의 값이 더이상 존재하지 않을 때
         dbms_output.put_line(v_dno || '번 부서의 최소급여 : ' || v_min || ' 최대급여 : ' || v_max || ' 평균급여 : ' || v_avg || ' 입니다.');
     end loop; 
     close c1;
@@ -47,7 +47,7 @@ begin
     loop
         fetch c1 into v_eno, v_ename, v_dname, v_loc;
         exit when c1%notfound;  
-        dbms_output.put_line('사원번호 : ' || v_eno || ' 사원이름 : ' || v_ename || ' 부서명 : ' || v_dname || ' 부서위치' || v_loc);
+        dbms_output.put_line('사원번호 : ' || v_eno || ' 사원이름 : ' || v_ename || ' 부서명 : ' || v_dname || ' 부서위치 : ' || v_loc);
     end loop; 
     close c1;
 end;
@@ -59,10 +59,10 @@ exec sp_eno_ename_dname_loc;
 	저장프로시져명 : sp_salary_b
     
 create or replace procedure sp_salary_b(
-    v_salary1 in employee.salary%type
+    v_salary1 in employee.salary%type   -- in, out 에서는 ; 처리하면 안됨.
 )
 is
-    v_ename employee.ename%type;
+    v_ename employee.ename%type;        -- v_emp employee%rowtype 모든 컬럼의 자료형을 선언
     v_salary2 employee.salary%type;
     v_job employee.job%type;
     cursor c1
@@ -73,25 +73,27 @@ is
 begin
     open c1;
     loop
-        fetch c1 into v_ename, v_salary2, v_job;
+        fetch c1 into v_ename, v_salary2, v_job;    -- into v_emp.ename, v_emp.salary, v_emp.job;
         exit when c1%notfound;  
-        dbms_output.put_line(v_salary1 || ' 보다 높은 급여는 받는사원이름 : ' || v_ename || ' 사원급여: ' || v_salary2 || ' 직책 : ' || v_job);
-    end loop; 
+        dbms_output.put_line(v_salary1 || ' 보다 높은 급여를 받는사원이름 : ' || v_ename || ' 사원급여: ' || v_salary2 || ' 직책 : ' || v_job);
+    end loop;                                                             -- v_emp.ename                v_emp.salary            v_emp.job
     close c1;
 end;
 /
 
-exec sp_salary_b(1000);
+exec sp_salary_b(2500);
 
 4. 인풋 매개변수 : emp_c10, dept_c10  두개를 입력 받아 각각 employee, department 테이블을 복사하는 저장프로시져를 생성하세요. 
 	저장프로시져명 : sp_copy_table
-
+    
+    -- PL/SQL 내부에서 익명 블럭에서 테이블을 생성 : sys 계정으로 grant create table to public; 테이블을 만들수 있는 권한을 부여해야한다.
+    -- 저장프로시저 실행 후 : revoke create table from public; (보안상 좋지않기때문)
 create or replace procedure sp_copy_table(   
-    v_name1 in varchar2,
+    v_name1 in varchar2,        -- 주의 : ';'을 넣으면 안됨, 자료형의 크기를 지정하면 안됨
     v_name2 in varchar2
 )
 is 
-    cursor1 INTEGER;
+    cursor1 INTEGER;    -- 커서 변수선언
     v_sql1 varchar2(100);
     cursor2 INTEGER;
     v_sql2 varchar2(100);
@@ -111,20 +113,45 @@ end;
 
 exec sp_copy_table('emp_c10' , 'dept_c10');
 
+-- 강사님ver
+create or replace procedure sp_ex4(
+    v_emp in varchar2, v_dept in varchar2)  -- 주의 : ';'를 넣으면 안됨, 자료형의 크기를 지정하면 안됨
+is
+    c1 integer;     -- 커서 변수선언
+    v_sql1 varchar2(500);   -- 테이블 생성 쿼리를 담을 변수
+    v_sql2 varchar2(500);
+begin
+    v_sql1 := 'create table ' || v_emp || ' as select * from employee';
+    v_sql2 := 'create table ' || v_dept || ' as select * from department';
+    
+    c1 := dbms_sql.open_cursor;         -- 커서 오픈
+    dbms_sql.parse(c1, v_sql1, dbms_sql.v7);
+    dbms_sql.parse(c1, v_sql2, dbms_sql.v7);
+    dbms_sql.close_cursor(c1);      -- 커서 종료
+    
+end;
+/
+
+exec sp_ex4('emp_c10', 'dept_c10');
+select * from emp_c10;
+select * from dept_c10;
+
 
 5. dept_c10 테이블에서 dno, dname, loc 컬럼의 값을 인풋 받아 인풋 받은 값을 insert하는 저장프로시져를 생성하시요. 
 	입력 값 : 50  'HR'  'SEOUL'
 	입력 값 : 60  'HR2'  'PUSAN' 
   
 create or replace procedure sp_dept_insert(
-    v_dno in department.dno%type,
-    v_dname in department.dname%type,
-    v_loc in department.loc%type
+    v_dno in dept_c10.dno%type,      
+    v_dname in dept_c10.dname%type,
+    v_loc in dept_c10.loc%type
 )
 is
 begin
     insert into dept_c10
-    values(v_dno, v_dname, v_loc);
+    values(v_dno, v_dname, v_loc);  
+    
+    dbms_output.put_line('정상적으로 잘 입력이 되었습니다');
 end;
 /
 
@@ -134,22 +161,25 @@ select * from dept_c10;
 commit;
 
 6. emp_c10 테이블에서 모든 컬럼의 값을 인풋 받아 인풋 받은 값을 insert하는 저장프로시져를 생성하시요. 
-	입력 값 : 8000  'SONG'    'PROGRAMER'  7788  sysdate  4500  1000  50 
+	입력 값 : 8000  'SONG'    'PROGRAMER'  7788  sysdate  4500  1000  50   -- 자료형때문에 PROGRAMMER 에서 M하나 빠짐
 
 create or replace procedure sp_emp_insert(
-    v_eno in employee.eno%type,
-    v_ename in employee.ename%type,
-    v_job in employee.job%type,
-    v_manager in employee.manager%type,
-    v_hiredate in employee.hiredate%type,
-    v_salary in employee.salary%type,
-    v_commission in employee.commission%type,
-    v_dno in employee.dno%type
+    v_eno in emp_c10.eno%type,
+    v_ename in emp_c10.ename%type,
+    v_job in emp_c10.job%type,
+    v_manager in emp_c10.manager%type,
+    v_hiredate in emp_c10.hiredate%type,
+    v_salary in emp_c10.salary%type,
+    v_commission in emp_c10.commission%type,
+    v_dno in emp_c10.dno%type
 )
 is
 begin
     insert into emp_c10
     values(v_eno, v_ename, v_job, v_manager, v_hiredate, v_salary, v_commission, v_dno);
+    
+    dbms_output.put_line('잘 입력 되었습니다');
+    
 end;
 /
 
@@ -157,6 +187,7 @@ exec sp_emp_insert(8000, 'SONG', 'PROGRAMER', 7788, sysdate, 4500, 1000, 50);
 
 select * from emp_c10;
 commit;
+
 7. dept_c10 테이블에서 4번문항의 부서번호 50의 HR 을 'PROGRAM' 으로 수정하는 저장프로시져를 생성하세요. 
 	<부서번호와 부서명을 인풋받아 수정하도록 하시오.> 
 	입력갑 : 50  PROGRAMER 
@@ -179,6 +210,22 @@ exec sp_chage(50, 'PROGRAMER');
 
 select * from dept_c10;
 
+-- 강사님ver
+create or replace procedure sp_ex7(
+    v_dno in dept_c10.dno%type,
+    v_dname in dept_c10.dname%type
+)
+is
+begin
+    update dept_c10
+    set dname = v_dname
+    where dno = v_dno;
+    
+    dbms_output.put_line('잘 업데이트 되었습니다');
+end;
+/
+exec sp_ex7(50, 'PROGRAMER');
+
 8. emp_c10 테이블에서 사원번호를 인풋 받아 월급을 수정하는 저장 프로시져를 생성하시오. 
 	입력 값 : 8000  6000
 
@@ -191,6 +238,8 @@ begin
     update emp_c10
     set salary = v_salary
     where eno = v_eno; 
+    
+    dbms_output.put_line('잘 수정되었습니다');
 end;
 /
 
@@ -228,10 +277,33 @@ exec sp_drop_table('emp_c10' , 'dept_c10');
 select * from emp_c10;
 select * from dept_c10;
 
+-- 강사님ver
+create or replace procedure sp_ex9(
+    v_emp in varchar2, v_dept in varchar2)  -- 주의 : ';'를 넣으면 안됨, 자료형의 크기를 지정하면 안됨
+is
+    c1 integer;     -- 커서 변수선언
+    v_sql1 varchar2(500);   -- 테이블 생성 쿼리를 담을 변수
+    v_sql2 varchar2(500);
+begin
+    v_sql1 := 'drop table ' || v_emp;
+    v_sql2 := 'drop table ' || v_dept;
+    
+    c1 := dbms_sql.open_cursor;         -- 커서 오픈
+    dbms_sql.parse(c1, v_sql1, dbms_sql.v7);
+    dbms_sql.parse(c1, v_sql2, dbms_sql.v7);
+    dbms_sql.close_cursor(c1);      -- 커서 종료
+    
+end;
+/
+
+exec sp_ex9('emp_c10', 'dept_c10');
+select * from emp_c10;
+select * from dept_c10;
+
 10. 사원이름을 인풋 받아서 사원명, 급여, 부서번호, 부서명, 부서위치을 OUT 파라미터에 넘겨주는 프로시저를 PL / SQL에서 호출
 
 create or replace procedure sp_info1(   
-    v_ename1 in varchar2,
+    v_ename1 in varchar2,   -- in employee.ename%type
     v_ename2 out varchar2,
     v_salary out number,
     v_dno out number,
@@ -247,22 +319,25 @@ begin
 end;
 /
 
-declare
-    var_ename2 varchar2(50);
+declare     -- OUT 파라미터 받을 변수선언
+    var_ename2 varchar2(50);    -- employee.ename%type;
     var_sal number;
     var_dno number;
     var_dname varchar2(50);
     var_loc varchar2(50);
-begin
+begin   -- 익명 블럭에서는 exec / execute 를 생략함.
     sp_info1('SCOTT', var_ename2, var_sal, var_dno, var_dname, var_loc);   
-    dbms_output.put_line('조회결과 : ' || var_ename2 || '   ' || var_sal || '   ' || var_dno || '   ' || var_dname || '   ' || var_loc);
+    
+    dbms_output.put_line('사원명   월급   부서번호   부서명   부서위치');
+    dbms_output.put_line('-----------------------------------------');
+    dbms_output.put_line(var_ename2 || '   ' || var_sal || '   ' || var_dno || '   ' || var_dname || '   ' || var_loc);
 end;
 /
 
 11. 사원번호를 받아서 사원명, 급여, 직책, 부서명, 부서위치을 OUT 파라미터에 넘겨주는 프로시저를 PL / SQL에서 호출
 
 create or replace procedure sp_info2(
-    v_eno in number,
+    v_eno in number,        -- in employee.eno%type
     v_ename out varchar2,
     v_sal out number,
     v_job out varchar2,
@@ -286,6 +361,9 @@ declare
     var_loc varchar2(50);
 begin
     sp_info2(7788, var_ename, var_sal, var_job, var_dname, var_loc);   
-    dbms_output.put_line('조회결과 : ' || var_ename || '   ' || var_sal || '   ' || var_job || '   ' || var_dname || '   ' || var_loc);
+    
+    dbms_output.put_line('사원명   월급   직책   부서명   부서위치');
+    dbms_output.put_line('---------------------------------------');
+    dbms_output.put_line(var_ename || '   ' || var_sal || '   ' || var_job || '   ' || var_dname || '   ' || var_loc);
 end;
 /
